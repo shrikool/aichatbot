78f22f2268121760d21974eb83634d1b61bf15aa
Shrikant Pandit <shrikool@gmail.com>
2016-06-10 17:56:42 +0530

    Adding Rest Controller

--- src/main/java/web/controller/ChatController.java
+++ src/main/java/web/controller/ChatController.java
@@ -1,7 +1,18 @@
 package web.controller;
 
+import model.Message;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * Created by shrikant
+ * Created by aichatbotteam
  */
+@RestController
 public class ChatController {
+    @RequestMapping("/chat")
+    public String chat(@RequestParam(value = "msg", defaultValue = "") String msg){
+
+        return "hello";
+    }
 }
